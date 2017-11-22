85b26724bc32e3bb33c88e6fe947fc409bdf153b
Unknown <sapnabapat270@gmail.com>
2017-11-22 18:37:42 +0530

    Introduction to JSX and Rendering Elements

---
+++ src/app/App.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+/*
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World!!!
+            </div>
+        );
+    }
+}
+*/
+
+
+
+/*
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Header</h1>
+                <h2>Content</h2>
+                <p>This is the content!!!</p>
+            </div>
+        );
+    }
+}
+
+*/
+
+/*class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{1+1}</h1>
+            </div>
+        );
+    }
+}*/
+
+/*
+class App extends React.Component {
+    render() {
+        var i = 1;
+        return (
+            <div>
+                <h1>{i == 1 ? 'True!' : 'False'}</h1>
+            </div>
+        );
+    }
+}
+*/
+
+/*
+class App extends React.Component {
+    render() {
+        var myStyle = {
+            fontSize: 100,
+            color: '#FF0000'
+        }
+        return (
+            <div>
+                <h1 style = {myStyle}>Header</h1>
+            </div>
+        );
+    }
+}
+*/
+
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Big header and comments</h1>
+            </div>
+        );
+    }
+}
+export default App;
